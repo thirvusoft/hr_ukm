d7bb9723432c062d0a5c7f1815b04da3c127ca70
Chandana S P <95605193+ChandanaSPthirvusoft@users.noreply.github.com>
2022-07-04 16:57:55 +0530

    Update employee_bonus_tool.py

--- united_knitting_mills/ukm/doctype/employee_bonus_tool/employee_bonus_tool.py
+++ united_knitting_mills/ukm/doctype/employee_bonus_tool/employee_bonus_tool.py
@@ -7,8 +7,8 @@ class EmployeeBonusTool(Document):
 @frappe.whitelist()
 def employee_finder(bonus1,location):
 	employee_names=[]
-	a=frappe.db.get_all("Employee",filters={"designation":bonus1,'location':location},fields=["name", "employee_name"])
-	for name in a:
+	emp_list=frappe.db.get_all("Employee",filters={"designation":bonus1,'location':location},fields=["name", "employee_name"])
+	for name in emp_list:
 		employee_names.append(name)
 	return employee_names
 
