b3f7ca6a71df93b8cf19d0a0c29b837acd59a1d1
ChandanaSPthirvusoft <chandana@thirvusoft.in>
2022-07-13 14:52:38 +0530

    Workflow for salary structure and assignment

--- united_knitting_mills/after_install.py
+++ united_knitting_mills/after_install.py
@@ -7,6 +7,8 @@ from united_knitting_mills.ukm.custom_fields.hr.shift_type import create_shift_t
 from united_knitting_mills.ukm.custom_fields.payroll.payroll_settings import create_payroll_settings_fields
 from united_knitting_mills.ukm.custom_fields.hr.attendance import attendance_customisation
 from united_knitting_mills.ukm.custom_fields.assets.location import create_location_fields
+from united_knitting_mills.ukm.utils.python.workflow import workflow_document_creation
+from united_knitting_mills.ukm.utils.python.salary_assignment_workflow import assignment_workflow_document_creation
 
 def create_custom_fields():
     create_company_fields()
@@ -18,3 +20,5 @@ def create_custom_fields():
     create_shift_type_fields()
     attendance_customisation()
     create_location_fields()
+    workflow_document_creation()
+    assignment_workflow_document_creation()
---
+++ united_knitting_mills/ukm/utils/python/workflow.py
@@ -0,0 +1,63 @@
+import frappe
+def workflow_document_creation():
+    create_state()
+    create_action()
+    create_workflow_doc()
+
+def create_workflow_doc():
+    if frappe.db.exists('Workflow', 'Salary Structure'):
+        frappe.delete_doc('Workflow', 'Salary Structure')
+    workflow = frappe.new_doc('Workflow')
+    workflow.workflow_name = 'Salary Structure'
+    workflow.document_type = 'Salary Structure'
+    workflow.workflow_state_field = 'workflow_state'
+    workflow.is_active = 1
+    workflow.send_email_alert = 1
+
+    workflow.append('states', dict(
+        state = 'Approval Pending',doc_status=0, allow_edit = 'HR User'
+    ))
+    workflow.append('states', dict(
+        state = 'Approved by HR Manager',doc_status=1, allow_edit = 'HR Manager'
+    ))
+    workflow.append('states', dict(
+        state = 'Rejected by HR manager',doc_status=0, allow_edit = 'HR Manager'
+    ))
+
+    workflow.append('transitions', dict(
+        state = 'Approval Pending', action='Approve', next_state = 'Approved by HR Manager',
+        allowed='HR Manager', allow_self_approval= 1
+    ))
+    workflow.append('transitions', dict(
+        state = 'Approval Pending', action='Reject', next_state = 'Rejected by HR manager',
+        allowed='HR Manager', allow_self_approval= 1
+    ))
+    workflow.insert(ignore_permissions=True)
+    return workflow
+def create_state():
+    list=["Draft","Submitted","Rejected",'Approval Pending','Approved by HR Manager','Rejected by HR manager']
+    for row in list:
+        if not frappe.db.exists('Workflow State', row):
+            new_doc = frappe.new_doc('Workflow State')
+            new_doc.workflow_state_name = row
+            if(row=="Draft"):
+                new_doc.style="Warning"
+            if(row=="Submitted"):
+                new_doc.style="Success"
+            if(row=="Rejected"):
+                new_doc.style="Danger"
+            if(row=="Approved by HR Manager"):
+                new_doc.style="Success"
+            if(row=="Approval Pending"):
+                new_doc.style="Warning"
+            if(row=="Rejected by HR manager"):
+                new_doc.style="Danger"
+            new_doc.save()
+
+def create_action():
+    list=["Reject", "Submit", "Draft",'Approve', 'Action']
+    for row in list:
+        if not frappe.db.exists('Workflow Action Master', row):
+            new_doc = frappe.new_doc('Workflow Action Master')
+            new_doc.workflow_action_name = row
+            new_doc.save()
