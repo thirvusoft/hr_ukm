c40ff9852cd63f9f0487ad31d7763f771bee10e6
ChandanaSPthirvusoft <chandana@thirvusoft.in>
2022-09-09 13:37:59 +0530

    fix

--- united_knitting_mills/ukm/report/daily_attendance_report/daily_attendance_report.py
+++ united_knitting_mills/ukm/report/daily_attendance_report/daily_attendance_report.py
@@ -17,46 +17,40 @@ def execute(filters=None):
 
             
     report_data = frappe.db.sql(""" select
+    							designation,
                                 name,
                                 employee,
                                 employee_name,
                                 checkin_time,
                                 checkout_time,
-								designation,
                                 late_min,
                                 total_shift_count
                                 from `tabAttendance`
                                 {0} order by designation
                                 """.format(conditions))
 
-    designation_count = frappe.db.sql(""" select
-                                count(*)
-                                from `tabAttendance`
-                                where docstatus = 1 and attendance_date = '{0}' group by designation
-                                """.format(attendance_date))
 
     data = [list(i) for i in report_data]
-    matched_item=""
-    index = 0
-    if data[0][5]:
-        check = data[0][5]
 
-    for i in range (0,(len(data)+len(designation_count))-1,1):
-        if data[i][5] != check:
-            check = data[i][5]
-            data.insert(i,[f'<b>{check}</b>','','','','','','',''])
+    check =''
+    for i in range (0,len(data),1):
+        if data[i][0] != check:
+            if i<len(data)-1: check = data[i+1][0] 
+            data[i][0] = f'<b>{data[i][0]}</b>'
 
+        else:
+            data[i][0]=''
     columns = get_columns()
     return columns, data
  
 def get_columns():
     columns = [
+        _("Designation") + ":Data:100",
         _("Name") + ":Link/Attendance:200",
         _("Employee Code") + ":Link/Employee:200",
         _("Employee Name") + ":Data:200",
         _("In-Time") + ":Time:100",
         _("Out-Time") + ":Time:100",
-        _("Designation") + ":Link/Designation:100",
         _("Late Entry Minutes") + ":Data:200",
         _("Total Shift") + ":Float:100",
         ]
