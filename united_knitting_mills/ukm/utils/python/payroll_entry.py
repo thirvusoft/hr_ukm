f6a9ce92d2a493eb7802cb09df94dd7734920038
ChandanaSPthirvusoft <chandana@thirvusoft.in>
2022-10-07 15:52:18 +0530

    fix for payrollentry and staff shift

--- united_knitting_mills/ukm/utils/python/payroll_entry.py
+++ united_knitting_mills/ukm/utils/python/payroll_entry.py
@@ -3,4 +3,4 @@ from frappe.utils import nowdate,add_days
 def validate_to_date(doc,event):
     if (doc.end_date >= nowdate()):
         doc.end_date = add_days(nowdate(),-1)
-        frappe.msgprint(f'End Date cannot be greater than Yesterday')
+        frappe.msgprint(f'End Date cannot be greater than Yesterday ({doc.end_date}). Valid date is applied.')
