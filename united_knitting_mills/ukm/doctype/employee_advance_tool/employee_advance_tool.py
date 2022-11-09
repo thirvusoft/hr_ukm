44609e47c06a90e4dd0ba43a9a25c56d8d6ae023
Gokulnath17 <gokulnath@thirvusoft.in>
2022-11-09 16:16:28 +0530

    advance tool

--- united_knitting_mills/ukm/doctype/employee_advance_tool/employee_advance_tool.py
+++ united_knitting_mills/ukm/doctype/employee_advance_tool/employee_advance_tool.py
@@ -1,19 +1,33 @@
+import json
 import frappe
 
 from frappe.model.document import Document
 from frappe.custom.doctype.property_setter.property_setter import make_property_setter
-
+from united_knitting_mills.ukm.utils.python.employee_advance import advance_validation
 class EmployeeAdvanceTool(Document):
 	pass
 @frappe.whitelist()
-def employee_finder(advance1,location):
+def employee_finder(location,designation=None,department=None):
+	advance_percentage = frappe.db.get_single_value("United Knitting Mills Settings","advance_percentage")
 	employee_names=[]
-	emp_list=frappe.db.get_all("Employee",filters={"designation":advance1,'location':location},fields=["name", "employee_name"])
+	filters= frappe._dict()
+	filters.update({
+		"location":location
+	})
+	if department:
+		filters.update({"department":department})
+	if designation:
+		filters.update({"designation":designation})
+	emp_list=frappe.db.get_all("Employee",filters=filters,fields=["name", "employee_name"])
 	for name in emp_list:
+		name.update({
+			"eligible_amount":advance_validation(name['name']),
+			"current_advance":((advance_validation(name['name']) or 0 )*advance_percentage)/100
+		})
 		employee_names.append(name)
 	return employee_names
 @frappe.whitelist()
-def create_employee_advance(name,amount,date,payment_type):
+def create_employee_advance(name,amount,eligible_amount,date,payment_type):
 		make_property_setter(
 				'Employee Advance', "purpose", "reqd", 0, "Data", validate_fields_for_doctype=False
 		)
@@ -22,12 +36,12 @@ def create_employee_advance(name,amount,date,payment_type):
 		)
 		advance_doc=frappe.new_doc('Employee Advance')
 		advance_doc.employee = name
-		advance_doc.advance_amount = amount
+		advance_doc.advance_amount = json.loads(amount)
 		advance_doc.posting_date = date
 		advance_doc.exchange_rate = 1.0
+		advance_doc.eligible_amount = json.loads(eligible_amount) or 0
 		if payment_type=="Deduct from Salary":
 			advance_doc.repay_unclaimed_amount_from_salary=1
 		advance_doc.insert()
-		advance_doc.save()
 		advance_doc.submit()
 		frappe.db.commit()
--- united_knitting_mills/ukm/utils/python/employee_advance.py
+++ united_knitting_mills/ukm/utils/python/employee_advance.py
@@ -6,9 +6,9 @@ def advance_validation(employee):
     latest_salary_slip=frappe.get_all("Salary Slip",{"employee":employee,"docstatus":"1"},["name","end_date"],
                                     order_by="end_date desc",
                                     limit=1)
+    to_date=nowdate()
     if latest_salary_slip:
         from_date=latest_salary_slip[0].end_date + timedelta(days=1)
-        to_date=nowdate()
         previous_advance=0
         previous_advance= frappe.db.sql("""select sum(advance_amount) from `tabEmployee Advance`
                                             where employee='{0}' and posting_date between '{1}' and '{2}' and docstatus IN ("0","1")
@@ -20,7 +20,7 @@ def advance_validation(employee):
                         from `tabAttendance`
                         where employee=%s and attendance_date>=%s and attendance_date<=%s and docstatus = 1
 		        """, (employee,from_date,to_date), as_list = 1)
-        if emp_shift_amount and previous_advance!=0:
+        if emp_shift_amount and previous_advance:
             return (emp_shift_amount[0][0] - previous_advance)
         elif emp_shift_amount:
             return (emp_shift_amount[0][0])
@@ -28,16 +28,16 @@ def advance_validation(employee):
         emp_shift_amount = frappe.db.sql("""
                         select sum(total_shift_amount)
                         from `tabAttendance`
-                        where employee=%s and attendance_date>=%s and attendance_date<=%s and docstatus = 1
-		        """, (employee,from_date,to_date), as_list = 1)
+                        where employee=%s and attendance_date<=%s and docstatus = 1
+		        """, (employee,to_date), as_list = 1)
        
         if emp_shift_amount:
             return (emp_shift_amount[0][0])
         
         
 def validate(doc,event):
-    if doc.advance_amount and not doc.advance_amount==0:
-        if doc.eligible_amount and not doc.eligible_amount==0 and not doc.eligible_amount<0:
+    if doc.advance_amount:
+        if doc.eligible_amount:
             if doc.advance_amount > doc.eligible_amount:
                 frappe.throw(f"Advance amount cannot be greater than {doc.eligible_amount}")
         else:
