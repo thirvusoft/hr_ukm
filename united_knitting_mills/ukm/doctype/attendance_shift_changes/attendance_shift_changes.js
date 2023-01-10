e17d69c169d044ab0819dc834c6ae58084018dc4
Gokulnath17 <gokulnath@thirvusoft.in>
2023-01-10 15:21:45 +0000

    live site changes

--- united_knitting_mills/ukm/doctype/attendance_shift_changes/attendance_shift_changes.js
+++ united_knitting_mills/ukm/doctype/attendance_shift_changes/attendance_shift_changes.js
@@ -2,19 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Attendance Shift Changes', {
-	onload: function(frm) {
-		if (cur_frm.doc.docstatus === 0){
-			frappe.db.get_list("Employee Checkin",{filters:{"attendance":cur_frm.doc.attendance}, fields:["time", "log_type"]}).then((list) => {
-				cur_frm.clear_table("checkin_details");
-				for (var i=0; i < list.length; i++){
-					let row = cur_frm.add_child("checkin_details");
-					row.time = list[i]["time"];
-					row.type = list[i]["log_type"];
-					cur_frm.refresh_field("checkin_details");
-					cur_frm.save()
-				}
-				
-			})
-		}
-	}
+	// onload: function(frm) {
+		
+	// }
 });
