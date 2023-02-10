49879f4db5fa4751af2fc5e4a58477ccec5040ce
ChandanaSPthirvusoft <chandana@thirvusoft.in>
2023-02-10 11:46:35 +0530

    feat:attendance report

---
+++ united_knitting_mills/ukm/report/monthly_attendance_report/monthly_attendance_report.js
@@ -0,0 +1,44 @@
+// Copyright (c) 2023, UKM and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Monthly Attendance Report"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"reqd":1,
+			"width": "80"
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"reqd":1,
+			"width": "80"
+		},
+		{
+			"fieldname":"department",
+			"label": __("Department"),
+			"fieldtype": "Link",
+			"options": "Department",
+			"width": "100",
+			"reqd":1
+		},
+		{
+			"fieldname":"designation",
+			"label": __("Designation"),
+			"fieldtype": "Link",
+			"options": "Designation",
+			"width": "100"
+		},
+		{
+			"fieldname":"unit",
+			"label": __("Unit"),
+			"fieldtype": "Link",
+			"options": "Location",
+			"width": "100"
+		},
+	]
+};
---
+++ united_knitting_mills/ukm/report/monthly_attendance_report/monthly_attendance_report.py
@@ -0,0 +1,156 @@
+from ctypes.wintypes import PFLOAT
+import frappe
+import pandas
+from datetime import date, datetime, timedelta
+from frappe import _
+from erpnext.education.report.student_monthly_attendance_sheet.student_monthly_attendance_sheet import daterange
+from frappe.utils.data import date_diff
+from erpnext.hr.utils import get_holiday_dates_for_employee
+
+def execute(filters=None):
+	columns, data = [], []
+	columns = get_columns(filters)
+	data = get_data(filters)
+	frappe.publish_realtime('refresh-report')
+	return columns, data
+
+def get_columns(filters):
+	columns = [
+		 {
+			"label": _("S.No"),
+			"fieldtype": "Data",
+			"fieldname": "sno",
+			"width": 50
+		},
+		{
+			"label": _("Employee ID"),
+			"fieldtype": "Link",
+			"fieldname": "code",
+			"options":"Employee",
+			"width": 110
+		},
+		{
+			"label": _("Name of Employee"),
+			"fieldtype": "Data",
+			"fieldname": "worker_name",
+			"width": 100
+		},
+		{
+			"label": _("Designation"),
+			"fieldtype": "Data",
+			"fieldname": "designation",
+			"width": 100
+		},
+		
+		]
+	from_date = filters["from_date"]
+	to_date = filters["to_date"]
+	from_d = tuple(map(int, from_date.split('-')))
+	to_d = tuple(map(int, to_date.split('-')))
+	between_dates = list(map(str, list(daterange(date(from_d[0], from_d[1], from_d[2]),date(to_d[0], to_d[1], to_d[2])))))
+	for i in between_dates:
+		
+		columns.append({
+
+			"label": i[len(i)-2:],
+			"fieldtype": "Data",
+			"fieldname": i,
+			"width": 70,
+		}),
+  
+
+	columns+=[
+				{
+			"label": _("Present Days"),
+			"fieldtype": "Data",
+			"fieldname": "present_days",
+			"width": 100
+		},
+		{
+			"label": _("Absent Days"),
+			"fieldtype": "Data",
+			"fieldname": "absent_days",
+			"width": 70
+		},
+		{
+			"label": _("Worked Sundays"),
+			"fieldtype": "Data",
+			"fieldname": "worked_sundays",
+			"width": 100
+		}
+	   
+	]
+	return columns
+def get_data(filters):
+	data=[]
+	from_date = filters["from_date"]
+	to_date = filters["to_date"]
+
+	filter = {'attendance_date':["between", (from_date, to_date)],'docstatus':1}
+	
+	keys = list(filters.keys())
+
+	from_d = tuple(map(int, from_date.split('-')))
+	to_d = tuple(map(int, to_date.split('-')))
+
+	between_dates = list(map(str, list(daterange(date(from_d[0], from_d[1], from_d[2]),date(to_d[0], to_d[1], to_d[2])))))
+   
+	if ("designation" in keys):
+		filter["designation"] = filters["designation"]
+
+	if ("department" in keys):
+		filter["department"] = filters["department"]
+
+	if ("unit" in keys):
+		filter["unit"] = filters["unit"]
+
+	attendance=frappe.db.get_all("Attendance", filters=filter, fields=["name","employee","employee_name","designation","unit", "total_shift_count", "total_shift_amount" ], 
+	group_by="employee",
+	order_by="designation")
+	no=1
+	for j in attendance:
+		f=frappe._dict()
+		out=frappe._dict()
+		tot = frappe._dict()
+		f.update(
+			{   "sno":str(no),
+				"code" : j['employee'],
+				"worker_name":j.employee_name,
+				"designation" : j.designation,
+				
+			}
+		)
+		shift=0
+		shift_amount=0
+		sunday = 0
+		for k in between_dates:
+			get_attendance=frappe.db.get_list("Attendance", {'employee':j.employee, 'workflow_state':"Present",'attendance_date':k}, ['checkin_time','checkout_time','total_shift_count'])
+			get_amount=frappe.db.get_value("Attendance", {'employee':j.employee, 'workflow_state':"Present",'attendance_date':k}, 'total_shift_amount')
+			sunday_attendance=frappe.db.get_value("Attendance", {'employee':j.employee, 'workflow_state':"Present",'attendance_date':k}, 'sunday_attendance')
+			if sunday_attendance:
+				sunday += 1
+			if get_attendance:
+				checkin = get_attendance[0]['checkin_time']
+				checkout = get_attendance[0]['checkout_time']
+				f.update({k:f'{checkin or "-"}'})
+				out.update({k:f'{checkout or "-"}'})
+				tot.update({k:f"<span style='color:green!important;font-weight:bold'>{get_attendance[0]['total_shift_count']}</span>"})
+				shift=shift+(get_attendance[0]['total_shift_count'] or 0)
+			else:
+				f.update({k:"-"})
+				out.update({k:"-"})
+				tot.update({k:f"<span style='color:red!important;font-weight:bold'>0</span>"})
+
+			shift_amount=shift_amount+(get_amount or 0)
+		payment_days = date_diff(to_date, from_date) + 1
+		holidays = get_holiday_dates_for_employee(j.employee,from_date, to_date)
+		payment_days -= len(holidays)
+			
+		tot.update({"present_days":f'<b>{shift}</b>',"absent_days":f'<b>{payment_days-shift}</b>',"worked_sundays":f'<b>{sunday}</b>'})
+
+		data.append(f)
+		data.append(out)
+		data.append(tot)
+		no+=1   
+	return data
+
